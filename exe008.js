371239eb60eba0c479def0e8477b578478d22924
Guto Rech <gutorech@me.com>
2024-06-05 15:17:20 +0200

    new-commit

---
+++ exe008.js
@@ -0,0 +1,10 @@
+console.log('Deu certo a mensagem')
+console.log('Segunda mensagem')
+
+let speed = 85
+
+if (speed > 80){
+    console.log(`Multado, sua velociade foi de ${speed} km/h`)
+} else {
+    console.log(`Tudo ok, sua velocidade foi de ${speed} km/h `)
+}
